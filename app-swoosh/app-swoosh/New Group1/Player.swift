c4741001fc6bffe7b13c5728e0bbaf08aafe62f1
Dennis Magnusson <dennis@Dennis-MacBook-Pro.local>
2017-09-15 22:41:18 +0300

    added model

---
+++ app-swoosh/app-swoosh/New Group1/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Dennis Magnusson on 15/09/2017.
+//  Copyright © 2017 Dennis Magnusson. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
